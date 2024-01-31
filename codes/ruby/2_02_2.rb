c71a88d77f0f017276fcf74905e145830400af56
takaheraw <takaheraw@gmail.com>
2024-02-01 05:21:02 +0900

    Update

---
+++ codes/ruby/2_02_2.rb
@@ -0,0 +1,6 @@
+a, b = gets.split.map(&:to_i) # a と b を入力する
+puts a & b # a AND b の値を出力する
+puts a | b # a OR b の値を出力する
+puts a ^ b # a XOR b の値を出力する
+puts [a, b].all? { |arg| arg == 1 } ? 0 : 1 # NAND
+puts [a, b].any? { |arg| arg == 1 } ? 0 : 1 # NOR
