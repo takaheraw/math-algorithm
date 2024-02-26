182d87b635c289d61617eb52d33c1721bdda5e9f
takaheraw <takaheraw@gmail.com>
2024-02-27 05:57:03 +0900

    マージソート

---
+++ summary/go/2_merge_sort.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, 0)
+
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] <= right[0] {
+			result = append(result, left[0])
+			left = left[1:]
+		} else {
+			result = append(result, right[0])
+			right = right[1:]
+		}
+	}
+
+	result = append(result, left...)
+	result = append(result, right...)
+
+	return result
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&arr[i])
+	}
+
+	sortedArr := mergeSort(arr)
+
+	fmt.Println(sortedArr)
+}
