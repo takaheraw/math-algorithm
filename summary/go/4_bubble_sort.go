f7da7dcd7804d2b65938f8c66240adba96b8dd7e
takaheraw <takaheraw@gmail.com>
2024-02-29 05:48:09 +0900

    バブルソート

---
+++ summary/go/4_bubble_sort.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func bubbleSort(arr []int) []int {
+	n := len(arr)
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+
+	return arr
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&arr[i])
+	}
+	sortedArr := bubbleSort(arr)
+	fmt.Println(sortedArr)
+}
