3084dde5548742b1fbed1bf882a633953bb288b8
takaheraw <takaheraw@gmail.com>
2024-03-22 04:57:04 +0900

    バブルソート

---
+++ summary/ts/4_bubble_sort.ts
@@ -0,0 +1,19 @@
+function bubbleSort(arr: number[]): number[] {
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // 要素を交換
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// 使用例
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = bubbleSort(unsortedArray);
+console.log(sortedArray); // [11, 12, 22, 25, 34, 64, 90]
