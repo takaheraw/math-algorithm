5a375ca91416e109d140031f771bd1f71fcf744a
takaheraw <takaheraw@gmail.com>
2024-04-10 06:28:11 +0900

    選択ソート

---
+++ summary/c++/1_selection_sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+void selectionSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        std::swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+    std::vector<int> arr = {64, 25, 12, 22, 11};
+    selectionSort(arr);
+    for (int i = 0; i < arr.size(); i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+    return 0;
+}
