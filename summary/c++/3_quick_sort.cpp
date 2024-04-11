3c4f7b0b0852994ab0dae1a21fd1a8a0e89103f4
takaheraw <takaheraw@gmail.com>
2024-04-12 04:21:28 +0900

    クイックソート

---
+++ summary/c++/3_quick_sort.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+
+void quickSort(std::vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pivot = partition(arr, low, high);
+
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
+
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+int main() {
+    std::vector<int> arr = {10, 7, 8, 9, 1, 5};
+    int n = arr.size();
+
+    quickSort(arr, 0, n - 1);
+
+    std::cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
