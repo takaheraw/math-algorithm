f7dd9702aec7854a00aa3be56fde852e44492387
takaheraw <takaheraw@gmail.com>
2024-06-19 04:51:05 +0900

    Union木

---
+++ summary/go/25_union_find.go
@@ -0,0 +1,48 @@
+package main
+
+type UnionFind struct {
+	parent, rank []int
+}
+
+func NewUnionFind(size int) *UnionFind {
+	parent := make([]int, size)
+	rank := make([]int, size)
+	for i := range parent {
+		parent[i] = i
+	}
+	return &UnionFind{parent, rank}
+}
+
+func (uf *UnionFind) Find(x int) int {
+	if uf.parent[x] != x {
+		uf.parent[x] = uf.Find(uf.parent[x])
+	}
+	return uf.parent[x]
+}
+
+func (uf *UnionFind) Union(x, y int) {
+	rootX := uf.Find(x)
+	rootY := uf.Find(y)
+	if rootX != rootY {
+		if uf.rank[rootX] > uf.rank[rootY] {
+			uf.parent[rootY] = rootX
+		} else if uf.rank[rootX] < uf.rank[rootY] {
+			uf.parent[rootX] = rootY
+		} else {
+			uf.parent[rootY] = rootX
+			uf.rank[rootX]++
+		}
+	}
+}
+
+func (uf *UnionFind) Connected(x, y int) bool {
+	return uf.Find(x) == uf.Find(y)
+}
+
+func main() {
+	uf := NewUnionFind(10)
+	uf.Union(1, 2)
+	uf.Union(2, 3)
+	println(uf.Connected(1, 3)) // true
+	println(uf.Connected(1, 4)) // false
+}
