f766d0c2e5e2be65941aa056e13e0bae9698502c
takaheraw <takaheraw@gmail.com>
2024-07-29 07:03:43 +0900

    選択ソート

---
+++ summary/python/1_selection_sort.py
@@ -0,0 +1,14 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+# Example usage:
+input_array = list(map(int, input().split()))
+sorted_array = selection_sort(input_array)
+print("Sorted Array:", sorted_array)
